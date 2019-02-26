ec9f4fd40cb650a71c177ac968ee526b9eca92c1
surya <surya@surya-setiawan.local>
2019-02-26 15:02:11 +0700

    New

--- app.js
+++ app.js
@@ -9,11 +9,19 @@ var fileupload = require('express-fileupload');
 
 const { Pool } = require('pg')
 
+// const pool = new Pool({
+//   user: 'postgres',
+//   host: 'localhost',
+//   database: 'pms',
+//   password: '12345',
+//   port: 5432,
+// })
+
 const pool = new Pool({
-  user: 'postgres',
-  host: 'localhost',
-  database: 'pms',
-  password: '12345',
+  user: 'vpdhpwcheyeuip',
+  host: 'ec2-107-20-185-27.compute-1.amazonaws.com',
+  database: 'd75s2eaqm34tgp',
+  password: 'c79b86048f9da1439d78ee6a1fcc7527d798478508e6533a34029c4fde59a07e',
   port: 5432,
 })
 
